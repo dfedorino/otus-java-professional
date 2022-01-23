c9e2947c5bc3e7c737489a5be780a076ac3fe462
dmitryfedorino <dmitriy.fedorino@electrolux.com>
2022-01-23 22:39:17 +0300

    Add dependency with 'implementation' configuration

--- L02-gradle02-appThatUsesLib/src/main/java/com/app_that_uses_lib/AppThatUsesLib.java
+++ L02-gradle02-appThatUsesLib/src/main/java/com/app_that_uses_lib/AppThatUsesLib.java
@@ -1,14 +1,18 @@
 package com.app_that_uses_lib;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Lists; // <- this dependency is marked as 'api' so it's available in this module
 
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
+// import static org.assertj.core.api.Assertions.assertThat; // <- this dependency is marked as 'implementation' so it's not available in this module
+
 public class AppThatUsesLib {
     public static void main(String[] args) {
         List<Integer> numbers = asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-        System.out.println("List " + numbers + " is reverted by a transitive guava dependency -> " + Lists.reverse(numbers));
+        List<Integer> reversedByGuava = Lists.reverse(numbers);
+        System.out.println("List " + numbers + " is reverted by a transitive guava dependency -> " + reversedByGuava);
+//        assertThat(reversedByGuava).isEqualTo(asList(9, 8, 7, 6, 5, 4, 3, 2, 1, 0));
     }
 }
