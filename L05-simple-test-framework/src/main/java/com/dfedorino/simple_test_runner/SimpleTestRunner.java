8984b813b9a02be5938a6f677a9971e131f5a677
dmitryfedorino <dmitriy.fedorino@electrolux.com>
2022-02-20 12:05:12 +0300

    Add parallel execution

--- L05-simple-test-framework/src/main/java/com/dfedorino/simple_test_runner/SimpleTestRunner.java
+++ L05-simple-test-framework/src/main/java/com/dfedorino/simple_test_runner/SimpleTestRunner.java
@@ -1,30 +1,45 @@
 package com.dfedorino.simple_test_runner;
 
+import com.dfedorino.simple_test_runner.simple_annotation.ParallelExecution;
+import com.dfedorino.simple_test_runner.simple_annotation.SimpleAfterEach;
+import com.dfedorino.simple_test_runner.simple_annotation.SimpleBeforeEach;
+import com.dfedorino.simple_test_runner.simple_annotation.SimpleTest;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SimpleTestRunner {
-    public String evaluateTestMethod(TestMethod testMethod, Object testClassInstance, List<TestMethod> befores, List<TestMethod> afters) {
+    private final String testReportDivider = Stream.generate(() -> "-").limit(100).collect(Collectors.joining());
+
+    public String evaluateTestClass(Class<?> testClass) {
+        List<TestMethod> testMethods = scanForAnnotatedMethods(testClass, SimpleTest.class);
+        List<TestMethod> befores = scanForAnnotatedMethods(testClass, SimpleBeforeEach.class);
+        List<TestMethod> afters = scanForAnnotatedMethods(testClass, SimpleAfterEach.class);
+        Stream<TestMethod> evaluationProcess = testMethods.stream();
+        if (testClass.isAnnotationPresent(ParallelExecution.class)) {
+            evaluationProcess =  evaluationProcess.parallel();
+        }
+        return evaluationProcess
+                .map(test -> evaluateTestMethod(testClass, befores, test, afters))
+                .collect(Collectors.joining(testReportDivider + System.lineSeparator(), testReportDivider + System.lineSeparator(), testReportDivider));
+
+    }
+
+    public String evaluateTestMethod(Class<?> testClass, List<TestMethod> befores, TestMethod testMethod, List<TestMethod> afters) {
+        Object testClassInstance = createTestClassInstance(testClass);
         StringBuilder report = new StringBuilder();
         try {
             for (TestMethod before : befores) {
                 before.evaluate(testClassInstance);
             }
             testMethod.evaluate(testClassInstance);
-            report.append("... PASSED ...")
-                    .append(System.lineSeparator())
-                    .append(String.format("Test %s with hash %s passed", testMethod.getTestMethodName(), testClassInstance.hashCode()));
+            appendPassedMessage(report, testMethod, testClassInstance);
         } catch (Throwable throwable) {
-            report.append("!!! FAILED !!!")
-                    .append(System.lineSeparator())
-                    .append(String.format("Test %s with hash %s failed:", testMethod.getTestMethodName(), testClassInstance.hashCode()))
-                    .append(System.lineSeparator())
-                    .append(String.format("Exception: %s", throwable.getClass().getSimpleName()))
-                    .append(System.lineSeparator())
-                    .append(String.format("Reason: %s", throwable.getMessage()));
+            appendFailedMessage(report, testMethod, testClassInstance, throwable);
         } finally {
             for (TestMethod after : afters) {
                 try {
@@ -35,6 +50,24 @@ public class SimpleTestRunner {
             }
         }
         return report.toString();
+    }
+
+    private void appendPassedMessage(StringBuilder report, TestMethod testMethod, Object testClassInstance) {
+        report.append("... PASSED ...")
+                .append(System.lineSeparator())
+                .append(String.format("Test %s with hash %s passed", testMethod.getTestMethodName(), testClassInstance.hashCode()))
+                .append(System.lineSeparator());
+    }
+
+    private void appendFailedMessage(StringBuilder report, TestMethod testMethod, Object testClassInstance, Throwable throwable) {
+        report.append("!!! FAILED !!!")
+                .append(System.lineSeparator())
+                .append(String.format("Test %s with hash %s failed:", testMethod.getTestMethodName(), testClassInstance.hashCode()))
+                .append(System.lineSeparator())
+                .append(String.format("Exception: %s", throwable.getClass().getSimpleName()))
+                .append(System.lineSeparator())
+                .append(String.format("Reason: %s", throwable.getMessage()))
+                .append(System.lineSeparator());
     }
 
     public Object createTestClassInstance(Class<?> testClass) {
